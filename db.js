9eb60145f09471ab775484ba17873c592fb8cc3c
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-07-11 14:20:33 +0200

    create db.js

---
+++ db.js
@@ -0,0 +1,17 @@
+// db.js
+import pg from 'pg'
+import dotenv from 'dotenv'
+//.env connection
+dotenv.config()
+
+const DBURL = process.env.DBURL
+
+const { Pool } = pg
+// Database connection parameters
+const db = new Pool({
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionString: DBURL
+})
+export default db
