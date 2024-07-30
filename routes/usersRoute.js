30757c4092f2bc3a3de3ec0d84205f215b514273
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-07-30 14:09:25 +0200

    replace dummy data with data base connection in users route

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,9 +1,17 @@
 import express from 'express'
+import db from '../db.js' // import the database connection
+
 const router = express.Router()
 
 // Define routes
-router.get('/users', (req, res) => {
-  res.send('Hello from the users!')
+router.get('/users', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM users') //query the database
+    res.json(rows) // respond with the data
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
 
 // Export the router to use it in your main app
