1e245c29db8e5e827cf985119c081e05f615e889
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-07-30 14:14:09 +0200

    repalce dammy data with database in cv route

--- routes/cvRoute.js
+++ routes/cvRoute.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import db from '../db.js'
 
 const router = express.Router()
 
 //Define routes
-router.get('/cv', (req, res) => {
-  res.send('Hello form cv!')
+router.get('/cv', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM cv') // query the database
+    res.json(rows) // respond with the data
+  } catch (err) {
+    console.err(err.message)
+    res.json(err)
+  }
 })
 
 // Export the router to use it in your main app
