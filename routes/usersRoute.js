03030e5c56dc3053332ce7377664476cacb32356
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-07-30 19:20:25 +0200

    update the get users route using params

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -14,5 +14,19 @@ router.get('/users', async (req, res) => {
   }
 })
 
+router.get('/users/:user_id', async (req, res) => {
+  let user_id = req.params.user_id
+  console.log(user_id)
+  try {
+    const { rows } = await db.query(
+      `SELECT * FROM users WHERE user_id = ${user_id}`
+    )
+    res.json(rows[0])
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
+})
+
 // Export the router to use it in your main app
 export default router
